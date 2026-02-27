f7739f2b2d1aec58001091c311dc0016ca1c2859
haeun228 <123haeun@naver.com>
2026-02-27 20:04:11 +0900

    feat: Store 기본 구조 & Embeddable 적용
    - StoreId, Owner, StoreAddress, Rating Embeddable 적용
    - Rating 초기값 지정 및 평균/리뷰 계산 메서드 구현

---
+++ src/main/java/com/sparta/no1delivery/domain/store/domain/Store.java
@@ -0,0 +1,41 @@
+package com.sparta.no1delivery.domain.store.domain;
+
+import com.sparta.no1delivery.global.domain.BaseUserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@ToString
+@Getter
+@Table(name = "P_STORE")
+@Access(AccessType.FIELD)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends BaseUserEntity {
+
+    @EmbeddedId
+    private StoreId id;
+
+    @Embedded
+    private Owner owner;
+
+    @Column(length = 65, name = "store_name")
+    private String name;
+
+    @Column(length = 100, name = "description")
+    private String description;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Embedded
+    private StoreAddress address;
+
+    @Embedded
+    private Rating rating = new Rating(); // 초기값 지정
+
+}
