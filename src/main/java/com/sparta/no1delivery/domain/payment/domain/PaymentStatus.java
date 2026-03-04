0f2107ddd248ba0d1fa3d63b85ba2887c222c5e1
kimdh32022 <kimdh32022@naver.com>
2026-03-04 20:39:58 +0900

    feat: 주석 수정

--- src/main/java/com/sparta/no1delivery/domain/payment/domain/PaymentStatus.java
+++ src/main/java/com/sparta/no1delivery/domain/payment/domain/PaymentStatus.java
@@ -21,6 +21,7 @@ public enum PaymentStatus {
             throw new CustomException(ErrorCode.PAYMENT_ALREADY_PROCESSED);
         }
     }
+
     // 결제 실패 상태 체크 (상태가 이미 결제가 완료되었거나, 취소했거나, 부분 취소가 되었다면 실패가 되면 안됌)
     public void verifyAbortable(){
         if(this == PaymentStatus.DONE || this == PaymentStatus.CANCELLED || this == PaymentStatus.PARTIAL_CANCELLED){
