aa1c7a3fdd0b6afaac05413d4b5b8ec2ffe7bd27
kimdh32022 <kimdh32022@naver.com>
2026-03-04 14:39:45 +0900

    feat: 필요없는 import문 삭제

--- src/main/java/com/sparta/no1delivery/global/infrastructure/event/EventConfig.java
+++ src/main/java/com/sparta/no1delivery/global/infrastructure/event/EventConfig.java
@@ -2,6 +2,8 @@ package com.sparta.no1delivery.global.infrastructure.event;
 
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
