a1806f549586eecdf16534471d0c17f3af9cbb14
kimdh32022 <kimdh32022@naver.com>
2026-03-04 11:51:15 +0900

    feat: Events 설정 파일(사용할 Publisher 고정)

---
+++ src/main/java/com/sparta/no1delivery/global/infrastructure/event/EventConfig.java
@@ -0,0 +1,36 @@
+package com.sparta.no1delivery.global.infrastructure.event;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class EventConfig implements AsyncConfigurer {
+
+    private ApplicationEventPublisher publisher;
+
+    @Bean
+    public InitializingBean eventsInitializer() {
+        return () -> Events.setPublisher(publisher);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 기본 스레드 수
+        executor.setMaxPoolSize(10);// 최대 스레드 수
+        executor.setQueueCapacity(50); //대기 큐의 용량
+        executor.setThreadNamePrefix("Async-"); // 스레드 이름 접두사?
+        executor.initialize();
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
+    }
+}
--- src/main/java/com/sparta/no1delivery/global/infrastructure/event/Events.java
+++ src/main/java/com/sparta/no1delivery/global/infrastructure/event/Events.java
@@ -7,7 +7,7 @@ public class Events {
     // 필드 만들어 두기
     private static ApplicationEventPublisher publisher;
 
-    static void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+    static void setPublisher(ApplicationEventPublisher publisher) {
         Events.publisher = publisher;
     }
 
