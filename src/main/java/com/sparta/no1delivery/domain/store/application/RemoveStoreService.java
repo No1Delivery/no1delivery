f6ffead29b41df557bf39b09d3c4e93d85c58520
haeun228 <123haeun@naver.com>
2026-03-05 19:17:15 +0900

    feat: RemoveStoreService 구현

---
+++ src/main/java/com/sparta/no1delivery/domain/store/application/RemoveStoreService.java
@@ -0,0 +1,32 @@
+package com.sparta.no1delivery.domain.store.application;
+
+import com.sparta.no1delivery.domain.store.domain.Store;
+import com.sparta.no1delivery.domain.store.domain.StoreId;
+import com.sparta.no1delivery.domain.store.domain.StoreRepository;
+import com.sparta.no1delivery.domain.store.domain.service.OwnerCheck;
+import com.sparta.no1delivery.global.domain.RoleCheck;
+import com.sparta.no1delivery.global.presentation.exception.CustomException;
+import com.sparta.no1delivery.global.presentation.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RemoveStoreService {
+
+    private final StoreRepository storeRepository;
+    private final RoleCheck roleCheck;
+    private final OwnerCheck ownerCheck;
+
+    @Transactional
+    public void remove(UUID storeId) {
+        Store store = storeRepository.findById(StoreId.of(storeId))
+                .orElseThrow(() -> new CustomException(ErrorCode.STORE_NOT_FOUND));
+
+        store.remove(roleCheck, ownerCheck);
+    }
+
+}
