28e58fcf7477389e35077c3d472ea7115b797c7b
DDoori <kdr0928@naver.com>
2026-03-07 23:06:14 +0900

    feat: UserController 생성

---
+++ src/main/java/com/sparta/no1delivery/domain/user/presentation/UserController.java
@@ -0,0 +1,55 @@
+package com.sparta.no1delivery.domain.user.presentation;
+
+import com.sparta.no1delivery.domain.user.application.UserService;
+import com.sparta.no1delivery.domain.user.application.dto.OwnerRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    //회원 정보 관련
+
+    //배송지 관련
+
+    // 권한 관련
+
+    // 사장 권한 요청
+    @PostMapping("/{userId}/owner-request")
+    public void requestOwnerRole(
+            @PathVariable BigInteger userId,
+            @RequestBody OwnerRequestDto.Request request
+    ) {
+
+        userService.requestOwnerRole(userId, request);
+    }
+
+    // 사장 요청 목록 조회 (관리자)
+    @GetMapping("/owner-requests")
+    public List<OwnerRequestDto.Response> getOwnerRequests() {
+
+        return userService.getOwnerRequests();
+    }
+
+    // 권한 승인
+    @PostMapping("/{userId}/owner-approve")
+    public void approveOwnerRole(@PathVariable BigInteger userId) {
+
+        userService.approveOwnerRole(userId);
+    }
+
+    // 권한 거절
+    @PostMapping("/{userId}/owner-reject")
+    public void rejectOwnerRole(@PathVariable BigInteger userId) {
+
+        userService.rejectOwnerRole(userId);
+    }
+
+
+}
