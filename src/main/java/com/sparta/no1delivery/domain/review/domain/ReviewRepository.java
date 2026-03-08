83ef17e5ea3500a086e8c3df7b5c8ac157bf1943
soyeonnan <thduszbzb12@gmail.com>
2026-03-09 00:55:59 +0900

    Feat:ReviewOrderInfo VO 구현

--- src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewRepository.java
@@ -7,9 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review,ReviewId> {
-    // 특정 주문에 대한 리뷰가 이미 존재하는지 확인
-    Optional<Review> findByOrder_OrderId(UUID orderId);
 
-    // 작성자(userId)가 작성한 리뷰 목록 조회
-    List<Review> findAllByReviewerId(Long reviewerId);
 }
