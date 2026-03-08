42e1f311ef2982cb32fa804fa84969efb9eacc47
soyeonnan <thduszbzb12@gmail.com>
2026-03-09 00:26:12 +0900

    Feat: ReviewId VO 구현

--- src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewId.java
+++ src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewId.java
@@ -1,24 +1,27 @@
 package com.sparta.no1delivery.domain.review.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import java.io.Serializable;
+import lombok.*;
 import java.util.UUID;
 
 @Embeddable
-public class ReviewId implements Serializable {
-    private UUID value;
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewId {
 
-    protected ReviewId() {}
+    @Column(length = 45, name = "review_id")
+    private UUID id;
 
-    public ReviewId(UUID value) {
-        this.value = value;
-    }
-
+    // 새 리뷰를 처음 만들 때
     public static ReviewId of() {
-        return new ReviewId(UUID.randomUUID());
+        return ReviewId.of(UUID.randomUUID());
     }
 
-    public UUID getValue() {
-        return value;
+    // 이미 있는 UUID를 받아서 ReviewId를 생성
+    public static ReviewId of(UUID id) {
+        return new ReviewId(id);
     }
 }
