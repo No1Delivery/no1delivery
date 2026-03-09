d065d9d8e4cd32ce74cdc64070e320a2a5e99e6e
yeojin-c <orlina2001@naver.com>
2026-03-09 14:50:37 +0900

    feat: 주문 결제 이벤트 핸들러 추가

--- src/main/java/com/sparta/no1delivery/domain/order/application/event/PaymentApprovedEventHandler.java
+++ src/main/java/com/sparta/no1delivery/domain/order/application/event/PaymentApprovedEventHandler.java
@@ -1,4 +1,36 @@
 package com.sparta.no1delivery.domain.order.application.event;
 
+import com.sparta.no1delivery.domain.order.domain.Order;
+import com.sparta.no1delivery.domain.order.domain.OrderRepository;
+import com.sparta.no1delivery.domain.order.domain.event.OrderPaymentConfirmedEvent;
+import com.sparta.no1delivery.global.presentation.exception.CustomException;
+import com.sparta.no1delivery.global.presentation.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class PaymentApprovedEventHandler {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    @EventListener
+    public void handle(OrderPaymentConfirmedEvent event) {
+
+        UUID orderId = event.orderId();
+
+        log.info("결제 승인 이벤트 수신 orderId={}", orderId);
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
+
+        order.startPreparing();
+    }
 }
