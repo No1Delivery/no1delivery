fc85d5a2e3581f71d6ba3549bcf6e8797ed2253c
soyeonnan <thduszbzb12@gmail.com>
2026-03-09 12:19:05 +0900

    Fix:review 어노테이션name 수정+예외처리 수정

--- src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewContent.java
+++ src/main/java/com/sparta/no1delivery/domain/review/domain/ReviewContent.java
@@ -1,6 +1,8 @@
 package com.sparta.no1delivery.domain.review.domain;
 
 import com.sparta.no1delivery.domain.review.domain.exception.ReviewRateOutOfRangeException;
+import com.sparta.no1delivery.global.presentation.exception.CustomException;
+import com.sparta.no1delivery.global.presentation.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Lob;
@@ -32,7 +34,7 @@ public class ReviewContent {
     private void setScore(int score) {
         // 리뷰의 평점은 필수이며 1~5점 사이 선택
         if (score < 1 || score > 5) {
-            throw new ReviewRateOutOfRangeException(score);
+            throw new CustomException(ErrorCode.REVIEW_RATE_RANGE);
         }
 
         this.score = score;
