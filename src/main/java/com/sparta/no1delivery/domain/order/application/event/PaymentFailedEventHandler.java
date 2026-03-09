ba22dcb877f84e93ee79acd0bea0188e81a618c6
yeojin-c <orlina2001@naver.com>
2026-03-09 16:01:37 +0900

    feat: 주문도메인 결제 이벤트 핸들러 추가

--- src/main/java/com/sparta/no1delivery/domain/order/application/event/PaymentFailedEventHandler.java
+++ src/main/java/com/sparta/no1delivery/domain/order/application/event/PaymentFailedEventHandler.java
@@ -2,38 +2,40 @@ package com.sparta.no1delivery.domain.order.application.event;
 
 import com.sparta.no1delivery.domain.order.domain.Order;
 import com.sparta.no1delivery.domain.order.domain.OrderRepository;
-import com.sparta.no1delivery.domain.order.domain.event.OrderPaymentConfirmedEvent;
+import com.sparta.no1delivery.domain.payment.domain.event.PaymentFailedEvent;
 import com.sparta.no1delivery.global.presentation.exception.CustomException;
 import com.sparta.no1delivery.global.presentation.exception.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PaymentApprovedEventHandler {
+public class PaymentFailedEventHandler {
 
     private final OrderRepository orderRepository;
 
-    // 결제 성공 → 주문 조리 시작
-    @Transactional
+    //결제 실패 → 주문 취소
+    @Async
     @EventListener
-    public void handle(OrderPaymentConfirmedEvent event) {
+    @Transactional
+    public void handle(PaymentFailedEvent event) {
 
         UUID orderId = event.orderId();
 
-        log.info("결제 승인 이벤트 수신 orderId={}", orderId);
+        log.info("결제 실패 이벤트 수신 orderId={}", orderId);
 
         // 주문 조회
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 
-        // 조리 시작 상태로 변경
-        order.startPreparing();
+        // 주문 취소 처리
+        order.cancel();
     }
 }
