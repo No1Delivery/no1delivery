33f5b3952bd4b6107424db983b85de7ff70f0cbb
kimdh32022 <kimdh32022@naver.com>
2026-03-10 10:46:06 +0900

    feat : JPA-AUditing 기능 at, by 자동등록 수정 1차 테스트

--- src/main/java/com/sparta/no1delivery/global/infrastructure/persistence/JPAConfig.java
+++ src/main/java/com/sparta/no1delivery/global/infrastructure/persistence/JPAConfig.java
@@ -2,22 +2,35 @@ package com.sparta.no1delivery.global.infrastructure.persistence;
 
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.no1delivery.global.infrastructure.security.SecurityUserDetails;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@RequiredArgsConstructor
 public class JPAConfig {
 
         @PersistenceContext
         private EntityManager em;
 
+        private final SecurityUserDetails securityUserDetails;
+
         @Bean
         public JPAQueryFactory jpaQueryFactory() {
             return new JPAQueryFactory(em);
+        }
+
+        @Bean
+        public AuditorAware<Long> auditorProvider() {
+                return ()-> Optional.ofNullable(securityUserDetails.getId());
         }
 }
 
