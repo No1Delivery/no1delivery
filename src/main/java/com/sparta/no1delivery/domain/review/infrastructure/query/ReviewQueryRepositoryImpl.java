1c71852e768cffec53f5f753e7e0ad0bcba26207
soyeonnan <thduszbzb12@gmail.com>
2026-03-10 14:11:24 +0900

    Remove: 잘못 추가된 ReviewQueryRepositoryImpl 삭제

--- src/main/java/com/sparta/no1delivery/domain/review/infrastructure/query/ReviewQueryRepositoryImpl.java
+++ src/main/java/com/sparta/no1delivery/domain/review/infrastructure/query/ReviewQueryRepositoryImpl.java
@@ -1,23 +1,4 @@
 package com.sparta.no1delivery.domain.review.infrastructure.query;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.sparta.no1delivery.domain.review.domain.Review;
-import com.sparta.no1delivery.domain.review.domain.ReviewId;
-import com.sparta.no1delivery.domain.review.domain.query.ReviewQueryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-@RequiredArgsConstructor
-
-public class ReviewQueryRepositoryImpl implements ReviewQueryRepository {
-
-    private final JPAQueryFactory queryFactory;
-
-    @Override
-    public Optional<Review> findById(ReviewId reviewId) {
-
-    }
+public class ReviewQueryRepositoryImpl {
 }
