c00ad09cbc3d0722466a161b59277ced8460e813
soyeonnan <thduszbzb12@gmail.com>
2026-03-10 14:04:36 +0900

    Fix:OrderInfoProvider 메서드 수정

--- src/main/java/com/sparta/no1delivery/domain/review/infrastructure/query/ReviewQueryRepositoryImpl.java
+++ src/main/java/com/sparta/no1delivery/domain/review/infrastructure/query/ReviewQueryRepositoryImpl.java
@@ -1,4 +1,23 @@
 package com.sparta.no1delivery.domain.review.infrastructure.query;
 
-public class ReviewQueryRepositoryImpl {
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.no1delivery.domain.review.domain.Review;
+import com.sparta.no1delivery.domain.review.domain.ReviewId;
+import com.sparta.no1delivery.domain.review.domain.query.ReviewQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+
+public class ReviewQueryRepositoryImpl implements ReviewQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<Review> findById(ReviewId reviewId) {
+
+    }
 }
