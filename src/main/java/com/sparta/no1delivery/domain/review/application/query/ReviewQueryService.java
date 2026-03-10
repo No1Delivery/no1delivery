bf344a65be812d7bf5c940e0e2c83619401de543
soyeonnan <thduszbzb12@gmail.com>
2026-03-10 17:03:42 +0900

    Feat:presentation구현

--- src/main/java/com/sparta/no1delivery/domain/review/application/query/ReviewQueryService.java
+++ src/main/java/com/sparta/no1delivery/domain/review/application/query/ReviewQueryService.java
@@ -1,18 +1,15 @@
 package com.sparta.no1delivery.domain.review.application.query;
 
-import com.sparta.no1delivery.domain.review.domain.ReviewId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class ReviewQueryRepository {
+public class ReviewQueryService {
 
-    private final ReviewQueryRepository reviewQueryRepository;
+    private final ReviewQueryService reviewQueryService;
 
     // 리뷰 상세 조회
 
--- src/main/java/com/sparta/no1delivery/domain/review/presentation/ReviewController.java
+++ src/main/java/com/sparta/no1delivery/domain/review/presentation/ReviewController.java
@@ -1,4 +1,19 @@
 package com.sparta.no1delivery.domain.review.presentation;
 
+import com.sparta.no1delivery.domain.review.application.ReviewService;
+import com.sparta.no1delivery.domain.review.application.query.ReviewQueryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Review API", description = "V1 리뷰 관리 API (등록/수정/삭제/조회)")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/reviews")
 public class ReviewController {
+
+   private final ReviewService reviewService;
+   private final ReviewQueryService
+
 }
